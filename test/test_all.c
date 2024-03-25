fcd8e8a30f9c6c4746b6f5abf097e9c7fbf7d425
Jason Cairns <jason.cairns@auckland.ac.nz>
2024-03-25 16:39:51 +1300

    test for clear_buffer

--- test/test_all.c
+++ test/test_all.c
@@ -105,6 +105,7 @@ TEST_LIST = {
 	{ "write_buffer buffer element is equivalent to string less than buffer size", test_buffer_w_smaller_string },
 	{ "write_buffer returns with nonzero if string is equal or greater than buffer size", test_buffer_w_larger_string },
 	{ "multiple buffer writes are equivalent to the input strings", test_buffer_w_multiple_strings },
+	{ "clearing the buffer works as expected", test_clear_buffer },
 	{ "the string_parse() performs a parse", test_parse },
 	{ "multiple parses are allowed for", test_multiple_parses },
 	{ "sring_parse of text with no link is equivalent to make_text", test_parse_text },
